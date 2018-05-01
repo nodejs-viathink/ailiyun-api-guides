ad64af1547c6444f8663a1bbff94cb3247cf990a
jianxun <medusa.leee@gmail.com>
2018-05-01 15:19:29 +0800

    修改日志打印格式

--- aliyun-mq/src/main/java/com/jianxun/producer/SimpleMQProducerRunnable.java
+++ aliyun-mq/src/main/java/com/jianxun/producer/SimpleMQProducerRunnable.java
@@ -41,9 +41,9 @@ public class SimpleMQProducerRunnable implements Runnable{
         producer.start();
         System.out.println("SimpleMQProducer" + this.producerId + "started...");
         for (int i = 0; i < 10; i++) {
-            Message message = new Message(this.topic, this.tag, "simpleMQProducer message test".getBytes());
+            Message message = new Message(this.topic, this.tag, ("simpleMQProducer tag " + this.tag +" index-" + i).getBytes());
             // 设置代表消息的业务关键属性，请尽可能全局唯一。
-            String orderId = this.producerId + "-" + Math.random();
+            String orderId = this.producerId + "--" + i + "--" + Math.random();
             message.setKey(orderId);
             // 分区顺序消息中区分不同分区的关键字段。
             // 全局顺序消息，该字段可以设置为任意非空字符串。
@@ -51,7 +51,7 @@ public class SimpleMQProducerRunnable implements Runnable{
             try {
                 SendResult sendResult = producer.send(message, shardingKey);
                 assert sendResult != null;
-                System.out.println(new Date() + " Send mq message success! Topic is:" + this.topic + " tag is: " + this.tag + "msgId is: " + sendResult.getMessageId());
+                System.out.println(new Date() + " Send mq message success! Topic is:" + this.topic + ", tag is: " + this.tag + ", msgKey is: " + message.getKey());
             } catch (ONSClientException e) {
                 System.out.println("simpleMQProducer send message failed: " + this.topic);
                 //出现异常意味着发送失败，为了避免消息丢失，建议缓存该消息然后进行重试。
